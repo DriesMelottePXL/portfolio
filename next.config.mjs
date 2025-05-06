a78872430e7c918929e116605a181a4da3305d13
DriesMelottePXL <dries.melotte@student.pxl.be>
2025-05-06 21:49:19 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: '/DriesMelottePXL',
-  assetPrefix: '/DriesMelottePXL/',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   eslint: {
     ignoreDuringBuilds: true,
   },
